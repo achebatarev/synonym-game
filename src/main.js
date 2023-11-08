86b82bc2f9f21e7e67a9c4a58f1fb700bfa249a4
Alexandr Chebatarev <alexandr.chebatarev@gmail.com>
2023-11-08 12:35:31 -0500

    folder structure with testing

---
+++ src/main.js
@@ -0,0 +1,8 @@
+console.log("hello worl")
+function hello() {
+    return "Hello World";
+}
+
+module.exports = {
+    hello
+}
---
+++ tests/main.test.js
@@ -0,0 +1,5 @@
+const main = require('../src/main');
+
+test('testing test', () => {
+    expect(main.hello()).toBe("Hello World");
+});
